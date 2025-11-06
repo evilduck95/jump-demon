862402b3dbeba47217041f82272fe49aa16acf6d
Keiran <keiran.brown@yodel.co.uk>
2025-11-06 18:34:57 +0000

    Cleanup and reorganised files

---
+++ scripts/game.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var credits = $ui/credits
+
+func _on_credits_clicked(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		set_pause(true)
+
+func _on_credits_close(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		set_pause(false)
+
+func set_pause(state):
+	get_tree().paused = state
+	credits.visible = state
