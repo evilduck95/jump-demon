17c78e8e46134eb73a90846a4ae3ab0a4f843103
Keiran <keiranbrown95@gmail.com>
2026-01-03 12:36:30 +0000

    Updated to use mouse clicks for control

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @onready var credits = $"ui/pause screen"
+@onready var pause_button = $"ui/pause button"
 
 func _on_credits_clicked(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		set_pause(true)
+		pause_button.visible = false
 
 func _on_credits_close(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 		set_pause(false)
+		pause_button.visible = true
 
 func set_pause(state):
 	get_tree().paused = state
